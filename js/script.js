f85e15eed9cb8124adb6fc9081d2b6100d51d975
m1roh <hayromain.rh@gmail.com>
2017-04-19 15:33:40 +0200

    Commit initial : index.html, style.css, script.js

---
+++ js/script.js
@@ -0,0 +1,52 @@
+var apiUrl = 'https://en.wikipedia.org/w/api.php?';
+var parameters = {
+	action: 'opensearch',
+	limit: '10',
+	namespace: '0',
+	format: 'json'
+};
+
+function test() {
+	var search = $('#searchBox').val();
+	parameters.search = search;
+	buildApiUrl();
+	grabDatas();
+}
+
+function buildApiUrl() {
+	apiUrl += $.param(parameters);
+}
+
+var datas,
+	titres,
+	articles,
+	liens;
+
+function grabDatas() {
+	console.log(apiUrl);
+	$.ajax({
+		type: 'GET',
+		url: apiUrl,
+		dataType: 'jsonp',
+		success: function(data) {
+			titres = data[1];
+			articles = data[2];
+			liens = data[3];
+			for (var i = 0; i < titres.length; i++) {
+				$('#answers').append('<div id="answer' + (i + 1) + '"><a class="btn btn-default" href="' + liens[i] + '" id ="link' + i + '"><h2 id="title' + (i + 1) + '">' + titres[i] + '</h2><p id="text' + (i + 1) + '">' + articles[i] + '</p></a></div>');
+			}
+		},
+		error: function() {
+			console.log('Request Failed');
+		}
+	});
+}
+
+$(function() {
+	$('#searchArticleBtn').on('click', function() {
+		$('#searchDiv').append('<input type="text" id="searchBox" /><div id="searchBtns"><button class="btn" id="searchValid">OK</button><button class="btn" id="searchReset">Annuler</button></div>');
+		$('#searchValid').on('click', function() {
+			test();
+		});
+	});
+});
