86e255d93364d4ee8197d45704106d37be5fda9d
m1roh <hayromain.rh@gmail.com>
2017-04-19 21:09:45 +0200

    essais sur div sm

--- js/script.js
+++ js/script.js
@@ -20,8 +20,10 @@ function buildApiUrl() {
 var datas,
 	titres,
 	titresSm,
+	article,
 	articles,
 	articlesSm,
+	lien,
 	liens;
 
 function grabDatas() {
@@ -40,12 +42,31 @@ function grabDatas() {
 				$('#text' + (i+1)).append(articles[i]); 
 			}
 			for (var j = 3; j < titres.length; j++) {
-				$('#link' + (j+1)).attr('href', liens[j]);
+				lien = liens[j];
 				titresSm = titres[j].split(' ', 2).join(' ');
 				$('#title' + (j+1)).append(titresSm);
+				article = articles[j];
 				articlesSm = articles[j].slice(0, 45) + '...';
 				$('#text' + (j+1)).append(articlesSm);
+				$('.linkS').on('click', function(e) {
+					e.preventDefault();
+					if ($(this).parent().attr('class') == 'col-md-3') {
+						$(this).parent().attr('class', 'col-md-12');
+						$(this).attr('href', lien);
+						$(this).children('p').html(article);
+					} else {
+						window.location = $(this).href;
+					}
+					
+					/*$(this).parent().attr('class', 'col-md-12');
+					$(this).attr('href', lien);
+					$(this).children('p').html(article);
+					$(this).on('click', function(e) {
+						return true;
+					})*/
+				});
 			}
+			
 		},
 		error: function() {
 			console.log('Request Failed');
